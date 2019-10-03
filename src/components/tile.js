808503341b396f651fedea7bbe5bf0413f37f0bc
Daniel Gawne <danielgawne@gmail.com>
2019-10-03 16:46:49 -0400

    edit tile format

--- src/components/tile.js
+++ src/components/tile.js
@@ -5,13 +5,14 @@ class Tile extends Component {
     return(
     <div className="tile">
       <h4>{this.props.title}</h4>
+      <a href={this.props.link} target="_blank" rel="noopener noreferrer">Project Link</a>
       <p>{this.props.blurb}</p>
-      <div>
+      <div className="techUsed">
         <p>Technologies Used:</p>
-        {this.props.js && <i class="fab fa-js" style={{fontSize: 30}}></i>}
-        {this.props.react && <i class="fab fa-react" style={{fontSize: 30}}></i>}
+        {this.props.js && <i class="fab fa-js" style={{fontSize: 30, padding: 10}}></i>}
+        {this.props.react && <i class="fab fa-react" style={{fontSize: 30, padding: 10}}></i>}
       </div>
-      <a href={this.props.link} target="_blank" rel="noopener noreferrer">Click Here!</a>
+
     </div>
   )
   }
