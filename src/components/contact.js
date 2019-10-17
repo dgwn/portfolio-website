b7a5764f32ded67b3f1597d006b788ec2901efd5
Daniel Gawne <danielgawne@gmail.com>
2019-10-16 22:26:08 -0400

    add contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,19 @@ class Contact extends Component {
   render() {
     return(
       <div>
-        <h1>Under Construction</h1>
+        <div className="banner-text banner-blue">
+          <h3>Contact Me</h3>
+          <hr />
+          <p>Email: danielgawne@gmail.com</p>
+          <div className="social-links">
+            <a href="https://www.linkedin.com/in/dgwne/" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-linkedin-square"  style={{color: '#0077B5'}} />
+            </a>
+            <a href="https://github.com/dgwn" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-github-square"  style={{color: '#6e5494'}}/>
+            </a>
+          </div>
+        </div>
 
 
       </div>
