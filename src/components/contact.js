d484d216010da1223595f5d12cafd0b2bdab4504
Daniel Gawne <danielgawne@gmail.com>
2020-05-17 21:35:17 -0400

    change fontawesome icons to svg icons

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,31 +1,54 @@
-import React, {Component } from 'react';
+import React, { Component } from "react";
+
+import sprite from "../assets/sprite.svg";
 
 class Contact extends Component {
   render() {
-    return(
+    return (
       <div>
         <div className="banner-text banner-blue">
           <h3>Contact Me</h3>
           <hr />
           <p>Email: danielgawne@gmail.com</p>
 
-
-
           <div className="social-links">
-            <a href="https://www.linkedin.com/in/dgwne/" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-linkedin-square"  style={{color: 'black'}} />
+            <a
+              href="https://www.linkedin.com/in/dgwne/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg className="svg_icon">
+                <use href={sprite + "#icon-linkedin"}></use>
+              </svg>
             </a>
-            <a href="https://www.linkedin.com/in/dgwne/" target="_blank" rel="noopener noreferrer"><p>LinkedIn Profile</p></a>
-            <a href="https://github.com/dgwn" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-github-square"  style={{color: 'black'}}/>
+            <a
+              href="https://www.linkedin.com/in/dgwne/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p>LinkedIn Profile</p>
             </a>
-            <a href="https://github.com/dgwn" target="_blank" rel="noopener noreferrer" text="Here"><p>Github Profile</p></a>
+            <a
+              href="https://github.com/dgwn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg className="svg_icon">
+                <use href={sprite + "#icon-github"}></use>
+              </svg>
+            </a>
+            <a
+              href="https://github.com/dgwn"
+              target="_blank"
+              rel="noopener noreferrer"
+              text="Here"
+            >
+              <p>Github Profile</p>
+            </a>
           </div>
         </div>
-
-
       </div>
-    )
+    );
   }
 }
 
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,41 +1,82 @@
-import React, {Component } from 'react';
-import {Grid, Cell} from 'react-mdl';
-import headshot from '../assets/headshot.jpg';
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import headshot from "../assets/headshot.jpg";
 
+import sprite from "../assets/sprite.svg";
 
 class Landing extends Component {
   render() {
-    return(
-      <div style={{width: '100%', margin: 'auto'}}>
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
         <Grid className="landing-grid">
           <Cell col={12}>
             <h1>Daniel Gawne</h1>
-            <img src={headshot} alt="headshot" className="avatar-img"/>
+            <img src={headshot} alt="headshot" className="avatar-img" />
             <div className="banner-text banner-blue">
               <h3>Full Stack Web Developer</h3>
               <hr />
-              <p>HTML/CSS  /  MERN Stack  /  Javascript  /   Design</p>
+              <p>HTML/CSS / MERN Stack / Javascript / Design</p>
               <div className="social-links">
-                <a href="https://www.linkedin.com/in/dgwne/" target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-linkedin-square"  style={{color: '#0077B5'}} title="LinkedIn"/>
+                <a
+                  href="https://www.linkedin.com/in/dgwne/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <svg className="svg_icon">
+                    <use href={sprite + "#icon-linkedin"}></use>
+                  </svg>
                 </a>
-                <a href="https://github.com/dgwn" target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-github-square"  style={{color: '#6e5494'}} title="Github"/>
+                <a
+                  href="https://github.com/dgwn"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <svg className="svg_icon">
+                    <use href={sprite + "#icon-github"}></use>
+                  </svg>
                 </a>
-                <a href="https://www.freecodecamp.org/dgwn" target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-free-code-camp"  style={{color: '#00471b'}} title="FreeCodeCamp"/>
+                <a
+                  href="https://www.freecodecamp.org/dgwn"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <svg className="svg_icon">
+                    <use href={sprite + "#icon-freecodecamp"}></use>
+                  </svg>
                 </a>
               </div>
             </div>
             <div className="banner-text banner-yellow">
               <h3>About Me</h3>
-              <p className="about-text">Welcome! I am a web developer from the Orlando area. My experience is mostly with the MERN stack (MongoDB, ExpressJS, ReactJS, NodeJS). I also work with HTML/CSS, Sass, Vanilla Javascript, UI/UX design, and prototype in Adobe XD and Photoshop. While coming from a background focused more in content than delivery, I have worked in the internet industry for at least 5 years and have done design work since 2010.</p>
-              <p className="about-text">I graduated from the University of Central Florida with a B.A. in Philosophy and a minor in Sociology. While my formal education does not originate in STEM, my research in my B.A. program focused largely on the role of technology and media on human labor and identity development. Additionally, my study of philosophy honed my analytic skills through the use of formal logic and the ability to assume the positions of others in order to gain a wider understanding of a topic. These tools are indispensible both for doing UX research and for writing clean, coherent, and consistent code. I hope to use my toolkit to work with clients such as small businesses and nonprofits in order to help them develop an online presence and address application needs.</p>
+              <p className="about-text">
+                Welcome! I am a web developer from the Orlando area. My
+                experience is mostly with the MERN stack (MongoDB, ExpressJS,
+                ReactJS, NodeJS). I also work with HTML/CSS, Sass, Vanilla
+                Javascript, UI/UX design, and prototype in Adobe XD and
+                Photoshop. While coming from a background focused more in
+                content than delivery, I have worked in the internet industry
+                for at least 5 years and have done design work since 2010.
+              </p>
+              <p className="about-text">
+                I graduated from the University of Central Florida with a B.A.
+                in Philosophy and a minor in Sociology. While my formal
+                education does not originate in STEM, my research in my B.A.
+                program focused largely on the role of technology and media on
+                human labor and identity development. Additionally, my study of
+                philosophy honed my analytic skills through the use of formal
+                logic and the ability to assume the positions of others in order
+                to gain a wider understanding of a topic. These tools are
+                indispensible both for doing UX research and for writing clean,
+                coherent, and consistent code. I hope to use my toolkit to work
+                with clients such as small businesses and nonprofits in order to
+                help them develop an online presence and address application
+                needs.
+              </p>
             </div>
           </Cell>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
