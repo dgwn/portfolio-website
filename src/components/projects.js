b89c8e549380dd5821e0f0adfa95566322b21e91
Daniel Gawne <danielgawne@gmail.com>
2021-06-19 17:22:47 -0400

    add blogging app to projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,15 +1,16 @@
-import React, {Component } from 'react';
-import Tile from './tile';
+import React, { Component } from "react";
+import Tile from "./tile";
 
 class Projects extends Component {
   render() {
     const PROJECTS = [
       {
         id: 0,
-        title: "Phonebook",
-        blurb: "Phonebook app with REST API, Node/Express backend and MongoDB. Made to fulfill Univ. of Helsinki's Full Stack Open coursework",
-        link: "https://github.com/dgwn/fullstackopen/tree/master/part3/phonebook",
-        liveLink: "https://phonebook-72648.herokuapp.com",
+        title: "Blog App",
+        blurb:
+          "Blogging app with REST API, React frontend, Node/Express backend and MongoDB. Made to fulfill Univ. of Helsinki's Full Stack Open coursework",
+        link:
+          "https://github.com/dgwn/fullstackopen/tree/master/part7/extended-bloglist",
         js: true,
         react: true,
         node: true,
@@ -17,8 +18,22 @@ class Projects extends Component {
       },
       {
         id: 1,
+        title: "Phonebook",
+        blurb:
+          "Phonebook app with REST API, Node/Express backend and MongoDB. Made to fulfill Univ. of Helsinki's Full Stack Open coursework",
+        link:
+          "https://github.com/dgwn/fullstackopen/tree/master/part3/phonebook",
+        liveLink: "https://phonebook-72648.herokuapp.com",
+        js: true,
+        react: true,
+        node: true,
+        db: true
+      },
+      {
+        id: 2,
         title: "Drum Machine",
-        blurb: "Fulfills FreeCodeCamp front-end-libraries project: \"Build a Drum Machine\"",
+        blurb:
+          'Fulfills FreeCodeCamp front-end-libraries project: "Build a Drum Machine"',
         link: "https://codepen.io/dgwn/pen/eYYZEOr",
         liveLink: "https://codepen.io/dgwn/pen/eYYZEOr",
         js: true,
@@ -27,9 +42,10 @@ class Projects extends Component {
         db: false
       },
       {
-        id: 2,
+        id: 3,
         title: "Markdown Previewer",
-        blurb: "Fulfills FreeCodeCamp front-end-libraries project: \"Build a Markdown Previewer\"",
+        blurb:
+          'Fulfills FreeCodeCamp front-end-libraries project: "Build a Markdown Previewer"',
         link: "https://codepen.io/dgwn/pen/XWWWxXZ",
         liveLink: "https://codepen.io/dgwn/pen/XWWWxXZ",
         js: true,
@@ -38,9 +54,10 @@ class Projects extends Component {
         db: false
       },
       {
-        id: 3,
+        id: 4,
         title: "Random Quote Generator",
-        blurb: "Fulfills FreeCodeCamp front-end-libraries project: \"Build a Random Quote Machine\"",
+        blurb:
+          'Fulfills FreeCodeCamp front-end-libraries project: "Build a Random Quote Machine"',
         link: "https://codepen.io/dgwn/pen/BaBgwBd",
         liveLink: "https://codepen.io/dgwn/pen/BaBgwBd",
         js: true,
@@ -49,7 +66,7 @@ class Projects extends Component {
         db: false
       },
       {
-        id: 4,
+        id: 5,
         title: "Etch Sketch",
         blurb: "Webpage with interactive cursor drawing",
         link: "https://github.com/dgwn/etch-sketch",
@@ -59,7 +76,7 @@ class Projects extends Component {
         db: false
       },
       {
-        id: 5,
+        id: 6,
         title: "Bookmarker",
         blurb: "Client side application to store URL bookmarks",
         link: "https://github.com/dgwn/bookmarker-web-app",
@@ -68,27 +85,26 @@ class Projects extends Component {
         node: false,
         db: false
       }
+    ];
 
-    ]
-
-    return(
+    return (
       <div class="tile-container">
-
-      {PROJECTS.map(function(item){
-        return (<Tile title={item.title}
-                      blurb={item.blurb}
-                      link={item.link}
-                      liveLink={item.liveLink}
-                      js={item.js}
-                      react={item.react}
-                      node={item.node}
-                      db={item.db}/>)
-
-      })}
-
-
+        {PROJECTS.map(function (item) {
+          return (
+            <Tile
+              title={item.title}
+              blurb={item.blurb}
+              link={item.link}
+              liveLink={item.liveLink}
+              js={item.js}
+              react={item.react}
+              node={item.node}
+              db={item.db}
+            />
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
