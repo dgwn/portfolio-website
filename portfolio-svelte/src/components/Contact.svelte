2e20bdffae01efea6535e97096b2cceb5e29410c
dgwn <danielgawne@gmail.com>
2026-06-05 15:16:07 -0500

    initial svelte migration

---
+++ portfolio-svelte/src/components/Contact.svelte
@@ -0,0 +1,31 @@
+<script>
+  import sprite from '../assets/sprite.svg';
+</script>
+
+<div>
+  <div class="banner-text banner-blue" id="contact-box">
+    <h1 class="subheading">Contact Me</h1>
+    <hr />
+    <p>Email: danielgawne@gmail.com</p>
+    <div class="social-links">
+      <a href="https://www.linkedin.com/in/dgwne/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn profile">
+        <svg class="svg_icon">
+          <use href="{sprite}#icon-linkedin"></use>
+        </svg>
+      </a>
+      <a href="https://www.linkedin.com/in/dgwne/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn profile">
+        <p>LinkedIn Profile</p>
+      </a>
+    </div>
+    <div class="social-links">
+      <a href="https://github.com/dgwn" target="_blank" rel="noopener noreferrer" aria-label="GitHub profile">
+        <svg class="svg_icon">
+          <use href="{sprite}#icon-github"></use>
+        </svg>
+      </a>
+      <a href="https://github.com/dgwn" target="_blank" rel="noopener noreferrer " aria-label="GitHub profile">
+        <p>Github Profile</p>
+      </a>
+    </div>
+  </div>
+</div>
